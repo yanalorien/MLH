e621e473af7578b32d9d7331247811563ea32e3a
yanalorien <yankaeva@gmail.com>
2020-11-03 22:30:50 -0500

    name field inputs

--- newTest/myTest.js
+++ newTest/myTest.js
@@ -181,10 +181,35 @@ describe('My Little Hero', function () {
             expect(value).toEqual('!(-_:.,? \'"');
         });
 
+        it('TC-034 Name field accepts 1 character', function (){
+            browser.url('');
+            const input = $(sel.name).setValue('a');
+            const value = $(sel.name).getValue();
+            expect(value).toEqual('a');
+        });
+
+        it('TC-035 Name field accepts 70 characters', function (){
+            browser.url('');
+            const input = $(sel.name).setValue('LpseiK43JntdLllt9NTaCP7wrkPOAvnbQwruzTElcj5UBKD6BnXDDmisSYWjp0FW3yV2Rq');
+            const value = $(sel.name).getValue();
+            expect(value).toEqual('LpseiK43JntdLllt9NTaCP7wrkPOAvnbQwruzTElcj5UBKD6BnXDDmisSYWjp0FW3yV2Rq');
+        });
+
+        // it('TC-036 Name field does not allow space character', function (){
+        //     browser.url('');
+        //     const input = $(sel.name).setValue(' ');
+        //     const value = $(sel.name).getValue();
+        //     expect(value).toEqual('');
+        // });
+
+        it('TC-037 Name field accepts 70 characters', function (){
+            browser.url('');
+            const input = $(sel.name).setValue('Tt2leKV8q06MODy8QVYznymdrVO6NCQ3hVvKg87VFuRa2HQ0xBqPxgwohEr5BN94tLyQJGk');
+            const value = $(sel.name).getValue();
+            expect(value).toEqual('LpseiK43JntdLllt9NTaCP7wrkPOAvnbQwruzTElcj5UBKD6BnXDDmisSYWjp0FW3yV2Rq');
+        });
 
 
-
-
-    });
+    });Tt2leKV8q06MODy8QVYznymdrVO6NCQ3hVvKg87VFuRa2HQ0xBqPxgwohEr5BN94tLyQJGk
 
 });
