9937ebec374cc51f7c693f142542497dceb44422
yanalorien <yankaeva@gmail.com>
2020-11-07 23:05:14 -0500

    name is displayed correctly in text

--- myTest/displayInText.js
+++ myTest/displayInText.js
@@ -4,18 +4,17 @@ const inputValues4 = require('../helpers/inputValues4.js');
 
 describe('data is displayed correctly in text', function () {
 
-    it('TC-021 Name displayed correctly in text', function () {
-        browser.url('');
-        inputValues4(data.name, data.gender.she, data.age, data.storyType);
-        const submit = $(sel.submit).click();
-        expect(submit).toEqual(true);
+    describe('Name data is displayed correctly in text', function () {
+
+        it('TC-021 Name displayed correctly in text', function () {
+            browser.url('');
+            inputValues4(data.name, data.gender.she, data.age, data.storyType);
+            const submit = $(sel.submit).click();
+            const finalText = $(sel.finalText).getText();
+            const text = finalText.includes(data.name);
+            expect(text).toEqual(true);
+        });
+
     });
 
-    // it('TC-021 Create button is clickable after 1-5 are filled in', function () {
-    //     browser.url('');
-    //     inputValues4(data.name, data.gender.she, data.age, data.storyType);
-    //
-    //     const submit = $(sel.submit).isEnabled();
-    //     expect(submit).toEqual(true);
-    // });
 });
