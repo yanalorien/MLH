06dcebdde16a94774b4cb136d4a9b3954c26c237
yanalorien <yankaeva@gmail.com>
2020-11-07 23:27:29 -0500

    age is shown correctly in text

--- myTest/displayInText.js
+++ myTest/displayInText.js
@@ -1,5 +1,7 @@
 const sel = require ('../data/selectors.json');
+const exp = require ('../data/expected.json');
 const data = require('../data/testData.json');
+const test = require('../data/testAge.json');
 const inputValues4 = require('../helpers/inputValues4.js');
 
 describe('data is displayed correctly in text', function () {
@@ -12,6 +14,19 @@ describe('data is displayed correctly in text', function () {
             const submit = $(sel.submit).click();
             const finalText = $(sel.finalText).getText();
             const text = finalText.includes(data.name);
+            expect(text).toEqual(true);
+        });
+
+    });
+
+    describe('Age data is displayed correctly in text', function () {
+
+        it('TC-021  displayed correctly in text', function () {
+            browser.url('');
+            inputValues4(test.name, test.gender.she, test.age.first, test.storyType);
+            const submit = $(sel.submit).click();
+            const finalText = $(sel.finalText).getText();
+            const text = finalText.includes(exp.ageTestFirst);
             expect(text).toEqual(true);
         });
 
