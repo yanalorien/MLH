6da2ead812c0398be8d40af2c787b05915922107
yanalorien <yankaeva@gmail.com>
2020-11-08 20:07:26 -0500

    try again button is correct

--- myTest/tryAgain.js
+++ myTest/tryAgain.js
@@ -1,5 +1,6 @@
 const sel = require ('../data/selectors.json');
 const data = require('../data/testData.json');
+const exp = require('../data/expected.json');
 const inputValues4 = require('../helpers/inputValues4andClick.js');
 
 describe('Try again button is present', function () {
@@ -9,6 +10,13 @@ describe('Try again button is present', function () {
         inputValues4(data.name, data.gender.she, data.age, data.storyType);
         const tryAgain = $(sel.tryAgain).isDisplayed();
         expect(tryAgain).toEqual(true);
+    });
+
+    it('TC-087 Image is displayed correctly at the story page', function () {
+        browser.url('');
+        inputValues4(data.name, data.gender.she, data.age, data.storyType);
+        const tryAgain = $(sel.tryAgain).getText();
+        expect(tryAgain).toEqual(exp.tryAgain);
     });
 
 });
