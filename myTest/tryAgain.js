86d550f9092c2bd55fdee9ab8784df0656f8ffb4
yanalorien <yankaeva@gmail.com>
2020-11-08 21:57:28 -0500

    try again button reloads the page

--- myTest/tryAgain.js
+++ myTest/tryAgain.js
@@ -3,20 +3,28 @@ const data = require('../data/testData.json');
 const exp = require('../data/expected.json');
 const inputValues4 = require('../helpers/inputValues4andClick.js');
 
-describe('Try again button is present', function () {
+describe('Try again button', function () {
 
-    it('TC-087 Image is displayed correctly at the story page', function () {
+    it('TC-087 Try again button is present', function () {
         browser.url('');
         inputValues4(data.name, data.gender.she, data.age, data.storyType);
         const tryAgain = $(sel.tryAgain).isDisplayed();
         expect(tryAgain).toEqual(true);
     });
 
-    it('TC-087 Image is displayed correctly at the story page', function () {
+    it('TC-087 Try again button is correct', function () {
         browser.url('');
         inputValues4(data.name, data.gender.she, data.age, data.storyType);
         const tryAgain = $(sel.tryAgain).getText();
         expect(tryAgain).toEqual(exp.tryAgain);
+    });
+
+    it('TC-087 Try again button reloads the page', function () {
+        browser.url('');
+        inputValues4(data.name, data.gender.she, data.age, data.storyType);
+        const tryAgain = $(sel.tryAgain).click();
+        const pageReloaded = $(sel.homePage).isDisplayed();
+        expect(pageReloaded).toEqual(true);
     });
 
 });
